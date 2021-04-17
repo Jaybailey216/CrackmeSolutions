0a9d120f454503bd3078e4dee0b8db2ac733cfd1
Josh <jaybailey216@gmail.com>
2021-04-17 14:53:51 -0400

    Added flotte

---
+++ flotte/pseudo.c
@@ -0,0 +1,21 @@
+// This challenge didn't require a keygen but here is my pseudo.c file that hopefully helps you understand what the code was doing
+
+float num1_3.564 = 3.564
+int user_input
+scanf("%d", &user_input) // 5
+int result
+result = num1_3.564 + (float)user_input // 5.0 + 3.564 = 8
+float i = 0
+while(result > i)
+{
+	num = result + i + num
+	i += .8
+}
+printf("%f", num)
+if(num == 4550.8)
+{
+	printf("Good password")
+	return 0;
+}
+printf("Bad password")
+return 0;
