397863cfff0f16f7ecfc251694eb5ed4b8b8fe3d
Josh <jaybailey216@gmail.com>
2021-05-02 00:50:21 -0400

    Added first ever crackme

---
+++ first_ever_crackme/boring_function.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void myFunc(string&);
+
+int main()
+{
+    string password;
+    string user_input = "";
+    password  = "1,)=8(";
+    int i = 0;
+    password += ";18,w";
+
+    cout << "Enter key: ";
+    cin >> user_input;
+    
+    cout << "\nPassword: " << password << endl;
+    myFunc(password);
+    if(user_input == password)
+    {
+        cout << "Congratulations!" << endl;
+    }
+    else
+        cout << "Too bad!" << endl;
+    
+    return 0;
+
+}
+
+void myFunc(string &test)
+{
+    int i = 0;
+    char key[] = "XYZ";
+    cout << "Before modifiying test: " << test << endl;
+    long size = test.size();
+    while(i < size - 1){
+        test[i] = test[i] ^ key[i % 3];
+        i++;
+    }
+    cout << "After modifying test: " << test << endl;
+
+}
